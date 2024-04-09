cb9f603e358817244f0e4185d69e720963430639
David L. Inman <davidlevere@gmail.com>
2024-04-09 18:12:59 -0400

    inc prog; remove yoke add in some parsing for csharp

--- src/parsers/csharp.rs
+++ src/parsers/csharp.rs
@@ -1,8 +1,4 @@
 
-use yoke::data::*;
-use yoke::parsing::*;
-use yoke::matching::*;
-
 pub enum CSharp {
     Class,
     Struct,
@@ -13,14 +9,35 @@ pub enum CSharp {
 
 pub fn parse(input : &str) -> Result<Vec<CSharp>, Box<dyn std::error::Error>> {
 
-    let ls = lexer::lex(input)?;
-    let bs = bracketer::bracket(ls.into_iter())?;
-    let output = bracket::process(&[], bs.into_iter())?;
 
-    Ok(output)
+    todo!()
 }
 
-fn blarg(input : &mut Vec<Lexeme>) {
+enum Lex {
+    Bracket(char),
+    Symbol(Box<str>),
+    String(Box<str>),
+}
+
+fn lex(mut input : &[char]) -> Result<Vec<Lex>, String> {
+    fn block_comment<'a>(mut input : &'a [char]) -> Result<&'a [char], Box<str>> {
+        loop {
+            match input { 
+                ['*', '/', rest @ ..] => { return Ok(rest); },
+                [_, rest @ ..] => { input = rest; },
+                [] => { return Err("Encountered end of line in block comment".into()); }
+            }
+        }
+    }
+
+    let mut ret = vec![];
+    loop {
+        match input {
+            ['/', '*', rest @ ..] => { input = block_comment(rest)?; },
+            
+            _ => todo!(),
+        }
+    }
 }
 
 #[cfg(test)]
