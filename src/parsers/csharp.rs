cd65f80baf562c1722f10cbb13ee9d4fdb43e30e
David L. Inman <davidlevere@gmail.com>
2024-06-16 17:44:31 -0400

    inc prog

--- src/parsers/csharp.rs
+++ src/parsers/csharp.rs
@@ -1,4 +1,6 @@
 
+use dealize::jerboa::*;
+
 pub enum CSharp {
     Class,
     Struct,
@@ -13,35 +15,15 @@ pub fn parse(input : &str) -> Result<Vec<CSharp>, Box<dyn std::error::Error>> {
     todo!()
 }
 
-enum Lex {
-    Bracket(char),
-    Symbol(Box<str>),
-    String(Box<str>),
-}
 
-fn lex(mut input : &[char]) -> Result<Vec<Lex>, String> {
-    fn block_comment<'a>(mut input : &'a [char]) -> Result<&'a [char], Box<str>> {
-        loop {
-            match input { 
-                ['*', '/', rest @ ..] => { return Ok(rest); },
-                [_, rest @ ..] => { input = rest; },
-                [] => { return Err("Encountered end of line in block comment".into()); }
-            }
-        }
-    }
-
-    let mut ret = vec![];
-    loop {
-        match input {
-            ['/', '*', rest @ ..] => { input = block_comment(rest)?; },
-            
-            _ => todo!(),
-        }
-    }
-}
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn should_parse_c_sharp() {
+
+    }
 
 }
