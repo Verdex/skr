7c6adecf67baf65cca8fd4262ab11cbe20fea0f9
David L. Inman <davidlevere@gmail.com>
2024-06-23 19:33:02 -0400

    inc prog c# parsing

--- src/parsers/csharp.rs
+++ src/parsers/csharp.rs
@@ -2,64 +2,61 @@
 use std::rc::Rc;
 use dealize::jerboa::*;
 
+#[derive(Clone)]
 pub enum CSharp {
     Class,
     Struct,
     Enum,
     Interface,
     Record,
+    Comment(Box<str>),
+    None,
 }
 
 static mut TOP_RULE : Option<Rc<Rule<char, CSharp>>> = None;
 
-static mut TOP_COMMENT : Option<Rc<Rule<char, Option<char>>>> = None;
+fn rule() -> Rc<Rule<char, CSharp>> {
 
-fn remove_comments(input : &str) -> Result<Vec<char>, Box<dyn std::error::Error>> {
+    unsafe { 
+        if TOP_RULE.is_none() {
+            let slash : Match<char, CSharp> = Match::pred(|c, _| *c == '/');
+            let star : Match<char, CSharp> = Match::pred(|c, _| *c == '*');
 
-    let r = unsafe { 
-        if TOP_COMMENT.is_none() {
-            let slash : Match<char, Option<char>> = Match::pred(|c, _| *c == '/');
-            let star : Match<char, Option<char>> = Match::pred(|c, _| *c == '*');
-            let end_line : Match<char, Option<char>> = Match::pred(|c, _| *c == '\r' || *c == '\n');
-
-            let anything : Rc<Rule<char, Option<char>>> = Rule::new
+            let end_line : Rc<Rule<char, CSharp>> = Rule::new
+            ( "end_line"
+            , vec![Match::pred(|c, _| *c == '\r' || *c == '\n')]
+            , |_| Ok(CSharp::None)
+            );
+            
+            let anything : Rc<Rule<char, CSharp>> = Rule::new
             ( "anything"
             , vec![ Match::pred(|c, _| true) ]
-            , |_| Ok(None)
+            , |_| Ok(CSharp::None) 
             );
 
-            let line_comment : Rc<Rule<char, Option<char>>> = Rule::new
+            let line_comment : Rc<Rule<char, CSharp>> = Rule::new
             ( "line_comment"
             , vec![ slash.clone()
                   , slash.clone()
-                  , Match::list(&anything)
+                  , Match::until(&anything, &end_line)
                   ]
-            , |_| Ok(None)
+            , |_| Ok(CSharp::None)
             );
 
-            TOP_COMMENT = Some(Rule::new("top", vec![], |_| Ok(None)));
-            Rc::clone(TOP_COMMENT.as_ref().unwrap())
+            TOP_RULE = Some(Rule::new("top", vec![], |_| Ok(CSharp::None)));
+            Rc::clone(TOP_RULE.as_ref().unwrap())
         }
         else {
-            Rc::clone(TOP_COMMENT.as_ref().unwrap())
+            Rc::clone(TOP_RULE.as_ref().unwrap())
         }
-    };
-    todo!()
+    }
 }
 
 pub fn parse(input : &str) -> Result<Vec<CSharp>, Box<dyn std::error::Error>> {
-    let top_rule = unsafe { 
-        if TOP_RULE.is_none() {
-            TOP_RULE = Some(Rule::new("blarg", vec![], |_| Ok(CSharp::Class)));
-            Rc::clone(TOP_RULE.as_ref().unwrap())
-        }
-        else {
-            Rc::clone(TOP_RULE.as_ref().unwrap())
-        }
-    };
+    let r = rule();
+    let input = input.chars().collect::<Vec<_>>();
 
-
-    todo!()
+    Ok(dealize::jerboa::parse(&input, r)?)
 }
 
 
